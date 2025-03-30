d823c041c2464f3895c2ec473139689cfdaff4f2
deepanshurwt06 <rwtd06@gmail.com>
2025-03-30 23:13:44 +0530

    complete home and about page and boiler plate code is ready

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,10 @@
 export default function About() {
     return (
-        <div>About</div>
+        <div className="px-4 py-12 max-w-2xl mx-auto">
+        <h1 className="text-center font-bold text-3xl mb-4 text-slate-800">Welcome to  About Page</h1>
+        <p className="mb-4 text-lg text-slate-700">This is a About page of my MERN authentication app . It is a type of boiler plate for all the application </p>
+       <p className="mb-4 text-lg text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias fuga eos voluptatum a quae eligendi ducimus nihil recusandae maxime saepe et autem, tempore quod magnam porro? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed id nam fuga perferendis exercitationem repellendus facilis debitis autem vel veniam! Neque architecto, dolorum distinctio totam reprehenderit eaque fugit nostrum iste, impedit et non?</p>
+       <p className="mb-4 text-lg text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias fuga eos voluptatum a quae eligendi ducimus nihil recusandae maxime saepe et autem, tempore quod magnam porro? Neque architecto, dolorum distinctio totam reprehenderit eaque fugit nostrum iste, impedit et non?</p>
+    </div>
     )
 }

9be16170cfc354097f0846def89db5a2283e0017
deepanshurwt06 <rwtd06@gmail.com>
2025-04-04 23:28:10 +0530

    search api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  
 
   return (
     <div className="bg-slate-400">
@@ -12,6 +11,15 @@ export default function Header() {
         <Link to={"/"}>
           <h1 className="text-3xl font-bold ">Auth App</h1>
         </Link>
+        
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input type="text" placeholder="Search... "
+          className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
+        </form>
+
         <ul className="flex gap-7">
           <Link to={"/"}>
             <li className="font-medium">Home</li>
@@ -23,11 +31,15 @@ export default function Header() {
           </Link>
 
           <Link to={"/profile"}>
-          {currentUser ? (
-            <img src={currentUser.profilePicture} alt="profile" className="h-8 w-8 rounded-full object-cover" />
-          ):(
-            <li className="font-medium">Sign in</li>
-          )}        
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-8 w-8 rounded-full object-cover"
+              />
+            ) : (
+              <li className="font-medium">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
